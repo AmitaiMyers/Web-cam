0f518983ff995d0bf5aaac60b78f5183b315bac0
AmitaiMyers <73966096+AmitaiMyers@users.noreply.github.com>
2023-02-15 12:06:16 +0200

    First commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import cv2, time,pandas
 from datetime import datetime
 
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
 #Create the data frame for start and end of seeing an object
 df = pandas.DataFrame(columns=["Start","End"])
 first_frame = None
@@ -34,6 +36,17 @@ while True:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
     status_list.append(status)
 
+    #Face recognition
+    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.05, minNeighbors=40)
+    for x, y, w, h in faces:
+        img = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        img = cv2.putText(frame, "Beautiful", (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 100, 255), 2)
+        # add a title to the face
+        cv2.imshow("Capturing", img)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Capturing", frame)
+
+
     #Record the time change from 0->1 or 1->0
     if status_list[-1] == 1 and status_list[-2] == 0:
         times.append(datetime.now())
@@ -41,9 +54,9 @@ while True:
         times.append(datetime.now())
 
 # Windows will appear
-    cv2.imshow("Capturing", gray)
-    cv2.imshow("Delta Frame", delta_frame)
-    cv2.imshow("Threshold Frame", thresh_frame)
+#     cv2.imshow("Capturing", gray)
+#     cv2.imshow("Delta Frame", delta_frame)
+#     cv2.imshow("Threshold Frame", thresh_frame)
     cv2.imshow("Color Frame", frame)
     key = cv2.waitKey(1)
 
@@ -52,49 +65,14 @@ while True:
         if status == 1:
             times.append(datetime.now())
         break
-    print(status)
-print(status_list)
+    # print(status)
+# print(status_list)
 print(times)
 
 # adding data to the csv
 for i in range(0,len(times),2):
-    df = df.append({"Start":times[i],"End":time[i+1]},ignore_index=True)
+    df = df.append({"Start":times[i],"End":times[i+1]},ignore_index=True)
 df.to_csv("Times.csv")
 video.release()
 cv2.destroyAllWindows
 
-
-#Face recognition
-# import cv2
-# face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-#
-# img = cv2.imread("photo2.jpg")
-# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#
-# faces = face_cascade.detectMultiScale(gray_img,
-#                                       scaleFactor=1.05,minNeighbors=40)
-# for x,y,w,h in faces:
-#     img = cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 3)
-#
-# resized = cv2.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)))
-#
-# cv2.imshow("Gray", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows
-
-#Video
-# import cv2, time
-#
-# video = cv2.VideoCapture(0)
-
-#check is a boolean
-#frame is a numpy array
-# check,frame = video.read()
-# # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-# time.sleep(3)
-# cv2.imshow("Capturing", frame)
-#
-#
-# cv2.waitKey(0)
-# video.release()
-# cv2.destroyAllWindows
